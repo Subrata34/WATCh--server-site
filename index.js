71a6de6c61077eb86f94e5c41f9b4a457542089e
Subrata34 <subratamodak3491@gmail.com>
2022-03-11 16:20:34 +0600

    files

--- index.js
+++ index.js
@@ -6,53 +6,50 @@ const { MongoClient } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
-
 //meddle wear
 app.use(express());
 app.use(cors());
 app.use(filesUpload());
 
 //mongodb connection
-const uri =
-  `mongodb+srv://watch:UzCIQ14mWuvFpjYy@cluster0.g4aj0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4aj0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  
 });
-console.log(uri);
-//function 
+//function
 
 //function
 async function run() {
   try {
     await client.connect();
-    const database = client.db('watch');
-    const watchCollection = database.collection('products');
+    const database = client.db("watch");
+    const watchCollection = database.collection("products");
 
-    
     app.post("/products", async (req, res) => {
-      products=req.body.products;
-      price=req.body.price;
-      discrption=req.body.discrption;
-      pic=req.files.image;
-      picData=pic.data;
-      encoded=picData.toString('base64');
-      encodedpic=Buffer.from(encoded,'base64');
-      const user={
+      products = req.body.products;
+      price = req.body.price;
+      discrption = req.body.discrption;
+      pic = req.files.image;
+      picData = pic.data;
+      encoded = picData.toString("base64");
+      encodedpic = Buffer.from(encoded, "base64");
+      const user = {
         products,
         discrption,
         price,
-        image:encoded
-      }
-      const personal= await  watchCollection.insertOne(user);
+        image: encoded,
+      };
+      const personal = await watchCollection.insertOne(user);
       res.json(personal);
     });
-  //get api 
-    app.get('/products',async(req,res)=>{
-      const couser=watchCollection.find({});
-      const watch=await couser.toArray();
+    //get api
+    app.get("/products", async (req, res) => {
+      const couser = watchCollection.find({});
+      const watch = await couser.toArray();
       res.send(watch);
-    })
+    });
   } finally {
   }
 }
